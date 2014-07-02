a428f70df7b4cfd4c445e02bad5769170baf3a56
hepoblet <hepoblet@alumnos.ubiobio.cl>
2014-07-02 01:22:08 -0400

    Doge

--- Assets/Scripts/MovimientoAuto.cs
+++ Assets/Scripts/MovimientoAuto.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class MovimientoAuto : MonoBehaviour {
 	public WheelCollider Llanta1, Llanta2, Llanta3, Llanta4;
-	public Transform Neumatico1, Neumatico2;
+	public Transform Neumatico1, Neumatico2, Neumatico3, Neumatico4;
 	public int Aceleracion = 100; 
 	public int desAceleracion = 80;
 	public float Velocidad;
 	public int VelocidadMaxima = 200;
+
+
+	public Texture Medidor;
+	public Texture Puntero;
+
+
+	int cuentaAngulo = 0;
 
 	void Start () {
 	
@@ -16,8 +23,18 @@ public class MovimientoAuto : MonoBehaviour {
 	}
 
 	void Update () {
-		Neumatico1.localEulerAngles = new Vector3 (0, -Llanta3.steerAngle*2, 0);
-		Neumatico2.localEulerAngles = new Vector3 (0, -Llanta4.steerAngle*2, 0);
+
+
+		Neumatico1.localEulerAngles = new Vector3 ((cuentaAngulo * Llanta1.rpm / 60 * 360 * Time.deltaTime), -Llanta3.steerAngle*2, 0);
+		Neumatico2.localEulerAngles = new Vector3 ((cuentaAngulo * Llanta2.rpm / 60 * 360 * Time.deltaTime), -Llanta4.steerAngle*2, 0);
+		cuentaAngulo++;
+		if (cuentaAngulo > 360) {
+			cuentaAngulo = 0;
+		}
+		//Girar Neumativos
+		Neumatico3.Rotate (new Vector3 (Llanta3.rpm / 60 * 360 * Time.deltaTime, 0, 0));
+		Neumatico4.Rotate (new Vector3 (Llanta4.rpm / 60 * 360 * Time.deltaTime, 0, 0));
+	
 
 		Velocidad = (2 * Mathf.PI * Llanta1.radius) * Llanta1.rpm * 60 / 1000;
 		Velocidad = Mathf.Round (Velocidad);
@@ -30,6 +47,7 @@ public class MovimientoAuto : MonoBehaviour {
 						//Aceleracion del Auto
 			Llanta1.motorTorque = Aceleracion * NuevosAxis ("Vertical");
 			Llanta2.motorTorque = Aceleracion * NuevosAxis ("Vertical");
+
 		} else {
 				
 			Llanta1.motorTorque = 0;
@@ -37,9 +55,12 @@ public class MovimientoAuto : MonoBehaviour {
 		
 		}
 		if (NuevosAxis ("Vertical") == 0) {
+
 			Llanta1.brakeTorque = desAceleracion;
 			Llanta2.brakeTorque = desAceleracion;
+
 		} else {
+
 			Llanta1.brakeTorque = 0;
 			Llanta2.brakeTorque = 0;
 		}
@@ -47,6 +68,8 @@ public class MovimientoAuto : MonoBehaviour {
 		//Rotacion del Auto
 		Llanta3.steerAngle = -10 * NuevosAxis ("Horizontal");
 		Llanta4.steerAngle = -10 * NuevosAxis ("Horizontal");
+
+
 	}
 
 
@@ -56,4 +79,15 @@ public class MovimientoAuto : MonoBehaviour {
 
 	}
 
+
+
+	void OnGUI(){
+
+		GUI.DrawTexture (new Rect (Screen.width-200, Screen.height -120, 200, 200), Medidor);
+		float Angulo = Mathf.Abs(Velocidad) * 180 / 320;
+
+		GUIUtility.RotateAroundPivot (Angulo, new Vector2(Screen.width -100,Screen.height -20));
+		
+		GUI.DrawTexture (new Rect (Screen.width-200, Screen.height -120, 200, 200), Puntero);
+	}
 }
